8ed4c05b33fef91388b4e4f42cc03002dfd25ec9
ivanovandrey78 <jaydenpull1@gmail.com>
2025-11-19 22:56:46 +0300

    feat: add PomodoroSession interface

--- year-1/semester-1/instrumental-tools/lab-3/pomodoro-timer/src/extension.ts
+++ year-1/semester-1/instrumental-tools/lab-3/pomodoro-timer/src/extension.ts
@@ -1,5 +1,12 @@
 import * as vscode from 'vscode';
 
+interface PomodoroSession {
+    type: 'work' | 'break';
+    duration: number;
+    timeLeft: number;
+    isActive: boolean;
+}
+
 export function activate() {
     vscode.window.showInformationMessage('Плагин запущен!');
 }
